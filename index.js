65f5bff51df9f01e39111c7f379ef28037b89e10
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-06-11 15:40:11 -0400

    express JS

--- index.js
+++ index.js
@@ -3,16 +3,6 @@ const exp = require('express');
 const app = exp()
 const port = process.env.PORT || 1337;
 
-const server = http.createServer(function(request, response) {
-
-    fs.readFile("index.html", function(err, data){
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(data);SR
-        response.end();
-    });
-
-}).listen(port);
-
 app.get('/', (req, res) => res.sendFile('index.html'))
 app.listen(port, () => console.log("Server running at http://localhost:%d", port));
 
