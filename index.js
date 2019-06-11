240923079736ce0f1d208f2c0bdbf8ca108a4d45
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-06-11 15:30:04 -0400

    serve

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const server = http.createServer(function(request, response) {
 
     fs.readFile("index.html", function(err, data){
         response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(data);
+        response.write(data);SR
         response.end();
     });
-    
+
 }).listen(port);
 
 
