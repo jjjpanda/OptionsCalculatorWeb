975207d350b48a47a17cce3386876caccd701293
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-06-11 16:28:37 -0400

    absolute path send

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const exp = require('express');
+const path = require('path')
 const app = exp()
 const port = process.env.PORT || 1337;
 
-app.get('/', (req, res) => res.sendFile('index.html'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
 app.listen(port, () => console.log("Server running at http://localhost:%d", port));
 
 
