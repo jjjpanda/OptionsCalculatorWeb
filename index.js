a0b4e3e554b5951e4e614c3460dffaedb3f280a3
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-06-11 15:29:31 -0400

    serve html

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const fs = require('fs');
 
 const port = process.env.PORT || 1337;
 const server = http.createServer(function(request, response) {
+
+    fs.readFile("index.html", function(err, data){
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+    });
+    
 }).listen(port);
 
-fs.readFile("index.html", function(err, data){
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write(data);
-    response.end();
-});
+
 
 console.log("Server running at http://localhost:%d", port);
