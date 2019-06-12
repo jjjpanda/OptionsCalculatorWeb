dd8102219fbd2c63e21328349e53d95488efd84f
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-06-11 22:45:11 -0400

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var ticker;
     $("#submit").click(function(){
       ticker=$("#ticker").val();
-      $.post("http://localhost:8181/",{ticker: ticker}, function(data){
+      $.post("/",{ticker: ticker}, function(data){
         if(data==='done')
           {
             alert("login success");
