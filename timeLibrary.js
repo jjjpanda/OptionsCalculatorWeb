b3e5129faaf77fb538de74d6ca68a05c6bc1c8ca
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-06-11 21:18:03 -0400

    updates

---
+++ timeLibrary.js
@@ -0,0 +1,14 @@
+const ms_per_day = 1000*60*60*24
+var current
+function getCurrentDate(){
+    current = new Date()
+}
+
+function timeTillExpiry(expiry){
+    const utc1 = Date.UTC(expiry.getFullYear(), expiry.getMonth(), expiry.getDate());
+    const utc2 = Date.UTC(current.getFullYear(), current.getMonth(), current.getDate());
+    return Math.floor((utc2-utc1)/ ms_per_day)
+}
+
+getCurrentDate()
+console.log(date)
