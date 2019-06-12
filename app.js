08929f08e71fc86a67298e7d55d29b09fe4b2613
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-06-11 23:16:18 -0400

    fix

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const key = process.env.tradier;(
 app.use(bodyParser.urlencoded({extended:false})));
 app.use(bodyParser.json());
 
-app.use('/js', exp.static(path.join(__dirname, 'js')));
+app.use('/js', exp.static(path.join(__dirname, '/js')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'temp.html')))
 app.post('/', function(req, res){
