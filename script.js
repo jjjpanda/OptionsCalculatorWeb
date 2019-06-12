979923166d71cec1699e40994a2df437f90b2c2c
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-06-11 22:55:01 -0400

    updates

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var ticker;
     $("#submit").click(function(){
       ticker=$("#ticker").val();
-      $.post("/",{ticker: ticker}, function(data){
+      $.post("app.js",{ticker: ticker}, function(data){
             alert(data)
       });
     });
