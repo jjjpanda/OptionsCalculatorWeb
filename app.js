5793c6dfef5095677efcd2ffd326c6f3aacbd327
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-06-11 23:12:46 -0400

    fix path

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const key = process.env.tradier;(
 app.use(bodyParser.urlencoded({extended:false})));
 app.use(bodyParser.json());
 
-app.use(exp.static('js'));
+app.use(exp.static(path.join(__dirname, 'js')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'temp.html')))
 app.post('/', function(req, res){
