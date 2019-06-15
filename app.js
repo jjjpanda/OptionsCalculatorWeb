82184684f7d4060643757599ec918a4415ca6037
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-06-15 00:28:29 -0400

    files creation

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.use('/js', exp.static(path.join(__dirname, '/js')));
+app.use('/jsLib', exp.static(path.join(__dirname, '/jsLib')));
 app.use('/css', exp.static(path.join(__dirname, '/css')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
