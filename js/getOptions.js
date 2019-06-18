d8c0ec8613b6d04bdab77bedb47f84e152555d2d
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-06-18 01:39:07 -0400

    updates

--- js/getOptions.js
+++ js/getOptions.js
@@ -165,8 +165,19 @@ function addCollapsers(){
         content.style.display = "none";
       } else {
         content.style.display = "block";
+        closeEverythingBut(i)
       }
     });
+  }
+}
+
+function closeEverythingBut(index){
+  var coll = document.getElementsByClassName("collapse");
+  for (i = 0; i < coll.length; i++) {
+    if(i != index){
+      coll[i].classList.toggle("active");
+      coll[i].nextSibling.style.display = 'none';
+    }
   }
 }
 
