ef4e7ebee9b289a4fb9c427360150b9b2a4ec77d
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-07-04 17:54:14 -0400

    updates

--- js/js.js
+++ js/js.js
@@ -40,14 +40,18 @@ app.controller("appController", function($scope){
 
     $scope.dataForChart = {
         dataset0: [
-          {x: 0, val_0: 0, val_1: 0, val_2: 0, val_3: 0},
-          {x: 1, val_0: 0.993, val_1: 3.894, val_2: 8.47, val_3: 14.347},
-          {x: 2, val_0: 1.947, val_1: 7.174, val_2: 13.981, val_3: 19.991},
-          {x: 3, val_0: 2.823, val_1: 9.32, val_2: 14.608, val_3: 13.509},
-          {x: 4, val_0: 3.587, val_1: 9.996, val_2: 10.132, val_3: -1.167},
-          {x: 5, val_0: 4.207, val_1: 9.093, val_2: 2.117, val_3: -15.136},
-          {x: 6, val_0: 4.66, val_1: 6.755, val_2: -6.638, val_3: -19.923},
-          {x: 7, val_0: 4.927, val_1: 3.35, val_2: -13.074, val_3: -12.625}
+          {x: expiryConvertToDate('2019-7-4'), val_0: 0.993, val_1: 3.894},
+          {x: expiryConvertToDate('2019-7-5'), val_0: 1.947, val_1: 7.174},
+          {x: expiryConvertToDate('2019-7-6'), val_0: 2.823, val_1: 9.32},
+          {x: expiryConvertToDate('2019-7-7'), val_0: 3.587, val_1: 9.996},
+          {x: expiryConvertToDate('2019-7-8'), val_0: 4.207, val_1: 9.093},
+          {x: expiryConvertToDate('2019-7-9'), val_0: 4.66, val_1: 6.7553},
+          {x: expiryConvertToDate('2019-7-10'), val_0: 4.927, val_1: 3.3525},
+          {x: expiryConvertToDate('2019-7-11'), val_0: 4.41, val_1: 3.3525},
+          {x: expiryConvertToDate('2019-7-12'), val_0: 4.51437, val_1: 3.125},
+          {x: expiryConvertToDate('2019-7-13'), val_0: 2.927, val_1: 3.23425},
+          {x: expiryConvertToDate('2019-7-14'), val_0: 4.243, val_1: 3.1525},
+          {x: expiryConvertToDate('2019-7-15'), val_0: 3.927, val_1: 3.3252},
         ]
       };
 
@@ -59,7 +63,7 @@ app.controller("appController", function($scope){
             key: "val_0",
             label: "An area series 1",
             color: "#1f77b4",
-            type: ['line', 'dot', 'area'],
+            type: ['line', 'dot'],
             id: 'mySeries1'
           }, 
           {
@@ -67,12 +71,24 @@ app.controller("appController", function($scope){
             dataset: "dataset0",
             key: "val_1",
             label: "An area series 2",
-            color: "#1f77b4",
-            type: ['line', 'dot', 'area'],
+            color: "#ff0000",
+            type: ['line', 'dot'],
             id: 'mySeries2'
           }
         ],
-        axes: {x: {key: "x"}}
+        tooltipHook: function(d){
+            return {
+              abscissas: "",
+              rows:  d.map(function(s){
+                return {
+                  label: s.series.label + " => " + dateToString(s.row.x) + " - ",
+                  value: s.row.y1,
+                  color: s.series.color
+                }
+              })
+            }
+          },
+        axes: {x: {key: "x", type: "date", ticks: "dataset0".length}}
       };
 
     $scope.getPrice = (showLoadingIcon) => {
