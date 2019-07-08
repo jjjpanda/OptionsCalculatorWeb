91889ba39abfdd16cc86810eaa91d9d8a22f861f
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2019-07-08 17:58:54 -0400

    updates iv calculation

--- js/optionsLibrary.js
+++ js/optionsLibrary.js
@@ -26,6 +26,42 @@ function cndf(x){
     return 0.5 * (1.0 + sign * y);
 }
 
+function cndfInv(p) {
+    var a1 = -39.6968302866538, a2 = 220.946098424521, a3 = -275.928510446969;
+    var a4 = 138.357751867269, a5 = -30.6647980661472, a6 = 2.50662827745924;
+    var b1 = -54.4760987982241, b2 = 161.585836858041, b3 = -155.698979859887;
+    var b4 = 66.8013118877197, b5 = -13.2806815528857, c1 = -7.78489400243029E-03;
+    var c2 = -0.322396458041136, c3 = -2.40075827716184, c4 = -2.54973253934373;
+    var c5 = 4.37466414146497, c6 = 2.93816398269878, d1 = 7.78469570904146E-03;
+    var d2 = 0.32246712907004, d3 = 2.445134137143, d4 = 3.75440866190742;
+    var p_low = 0.02425, p_high = 1 - p_low;
+    var q, r;
+    var icndf;
+
+    if ((p < 0) || (p > 1))
+    {
+        icndf = null;
+    }
+    else if (p < p_low)
+    {
+        q = Math.sqrt(-2 * Math.log(p));
+        icndf = (((((c1 * q + c2) * q + c3) * q + c4) * q + c5) * q + c6) / ((((d1 * q + d2) * q + d3) * q + d4) * q + 1);
+    }
+    else if (p <= p_high)
+    {
+        q = p - 0.5;
+        r = q * q;
+        icndf = (((((a1 * r + a2) * r + a3) * r + a4) * r + a5) * r + a6) * q / (((((b1 * r + b2) * r + b3) * r + b4) * r + b5) * r + 1);
+    }
+    else
+    {
+        q = Math.sqrt(-2 * Math.log(1 - p));
+        icndf = -(((((c1 * q + c2) * q + c3) * q + c4) * q + c5) * q + c6) / ((((d1 * q + d2) * q + d3) * q + d4) * q + 1);
+    }
+
+    return icndf;
+}
+
 function ndf(x){
     return 1 / Math.sqrt(2 * Math.PI) * Math.exp(-1 * x * x / 2);
 }
@@ -108,6 +144,16 @@ function calculateGreeks(t, priceUnderlying, strike, isCall, isLong, r, divYield
 //IV and Price
 function calculateIV(t, priceOfOption, priceUnderlying, strike, isCall, r, divYield){
     var iv = Math.sqrt(Math.PI * 2 / t) * priceOfOption/priceUnderlying
+    if(priceOfOption + strike < priceUnderlying && (isCall ? strike/priceUnderlying < 0.75 : strike/priceUnderlying > 1.33)){
+        if(isCall){
+            d = cndfInv(0.999/(Math.exp(-divYield * t)))
+        }
+        else if(!isCall){
+            d = cndfInv(-0.999/(Math.exp(-divYield * t)) + 1)
+        }
+        console.log('bruh')
+        return Math.abs((Math.sqrt(t*(2*Math.log(priceUnderlying/priceOfOption) + Math.pow(d,2) - 2*divYield*t + 2*r*t))-d*Math.sqrt(t))/t)
+    }
     var priceOfOptionTheoretical, vega;
     priceOfOptionTheoretical = calculateOptionsPrice(t, priceUnderlying, strike, isCall, true,  r, divYield, iv)
     stopTrying = 0
